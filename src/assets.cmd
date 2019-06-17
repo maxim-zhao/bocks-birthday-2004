be3f927e4c13afcdc1716f36c3b59f8eea2da78f
Maxim <maxim@smspower.org>
2019-06-17 21:50:57 +0100

    💚 Don't use paths in assets.cmd

--- src/assets.cmd
+++ src/assets.cmd
@@ -1,3 +1,3 @@
-for %%f in (backgrounds\*.png) do "..\..\..\Delphi\bmp2tile\bmp2tile.exe" "%%f" -tileoffset 0 -savetiles "%%f.tiles.zx7" -savetilemap "%%f.tilemap.zx7" -savepalette "%%f.palette.bin" -exit
-for %%f in (sprites\*.png) do "..\..\..\Delphi\bmp2tile\bmp2tile.exe" "%%f" -nomirror -savetiles "%%f.tiles.zx7" -savetilemap "%%f.tilemap.inc" -exit
-for %%f in (music\*.vg*) do java -jar "..\..\PSGLib\PSGlib-master\tools\PSGTool.jar" "%%f"
+for %%f in (backgrounds\*.png) do "bmp2tile.exe" "%%f" -tileoffset 0 -savetiles "%%f.tiles.zx7" -savetilemap "%%f.tilemap.zx7" -savepalette "%%f.palette.bin" -exit
+for %%f in (sprites\*.png) do "bmp2tile.exe" "%%f" -nomirror -savetiles "%%f.tiles.zx7" -savetilemap "%%f.tilemap.inc" -exit
+for %%f in (music\*.vg*) do java -jar "PSGTool.jar" "%%f"
